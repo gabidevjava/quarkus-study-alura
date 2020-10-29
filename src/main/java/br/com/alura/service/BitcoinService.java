6a3feb664b4cfeb376c22fc9b8bc03e54c50abf2
gabidevjava <zooiv3rde@gmail.com>
2020-10-29 14:41:58 -0300

    inject service

--- src/main/java/br/com/alura/service/BitcoinService.java
+++ src/main/java/br/com/alura/service/BitcoinService.java
@@ -3,11 +3,16 @@ package br.com.alura.service;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
 import br.com.alura.model.Bitcoin;
 
+@Path("/bitcoins")
+@RegisterRestClient
 public interface BitcoinService {
 	
 	@GET
