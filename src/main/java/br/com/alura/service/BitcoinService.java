f06c796dc84abbf4be3e8d0221ce834b4f22b581
gabidevjava <zooiv3rde@gmail.com>
2020-10-29 14:39:39 -0300

    add produces

--- src/main/java/br/com/alura/service/BitcoinService.java
+++ src/main/java/br/com/alura/service/BitcoinService.java
@@ -2,10 +2,16 @@ package br.com.alura.service;
 
 import java.util.List;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 import br.com.alura.model.Bitcoin;
 
 public interface BitcoinService {
 	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
 	public List<Bitcoin> listar();
 
 }
