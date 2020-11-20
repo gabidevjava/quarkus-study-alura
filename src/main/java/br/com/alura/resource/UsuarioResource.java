57066082937f6c3ac0f81c5f48c53a1301fad071
gabidevjava <zooiv3rde@gmail.com>
2020-11-20 14:40:07 -0300

    fix http code para post

--- src/main/java/br/com/alura/resource/UsuarioResource.java
+++ src/main/java/br/com/alura/resource/UsuarioResource.java
@@ -2,7 +2,7 @@ package br.com.alura.resource;
 
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 
@@ -11,7 +11,7 @@ import br.com.alura.model.Usuario;
 @Path("/usuarios")
 public class UsuarioResource {
 	
-	@GET
+	@POST
 	@Transactional
 	@Consumes(MediaType.APPLICATION_JSON)
 	public void inserir(Usuario usuario) {
