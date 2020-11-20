8bf175c79b291d8fa45feb191547914df4ecebaa
gabidevjava <zooiv3rde@gmail.com>
2020-11-20 14:38:04 -0300

    inserindo ordem no banco

--- src/main/java/br/com/alura/resource/OrdemResource.java
+++ src/main/java/br/com/alura/resource/OrdemResource.java
@@ -1,7 +1,13 @@
 package br.com.alura.resource;
 
+import java.time.LocalDate;
+
 import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
 
 import br.com.alura.model.Ordem;
 import br.com.alura.repository.OrdemRepository;
@@ -12,8 +18,13 @@ public class OrdemResource {
 	@Inject
 	OrdemRepository repository;
 	
+	@POST
+	@Transactional
+	@Consumes(MediaType.APPLICATION_JSON)
 	public void inserir(Ordem ordem) {
-		//ordem.set
+		ordem.setData(LocalDate.now());
+		ordem.setStatus("ENVIADO");
+		
 		repository.persist(ordem);
 	}
 
