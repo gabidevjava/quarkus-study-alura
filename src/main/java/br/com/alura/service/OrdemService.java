0583a4f5718d18b681a962d6f99b9d8659191d41
gabidevjava <zooiv3rde@gmail.com>
2020-11-30 17:55:14 -0300

    validando usuario logado

--- src/main/java/br/com/alura/service/OrdemService.java
+++ src/main/java/br/com/alura/service/OrdemService.java
@@ -1,12 +1,14 @@
 package br.com.alura.service;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.core.SecurityContext;
 
 import br.com.alura.model.Ordem;
+import br.com.alura.model.Usuario;
 import br.com.alura.repository.OrdemRepository;
 
 @ApplicationScoped
@@ -16,6 +18,14 @@ public class OrdemService {
 	OrdemRepository repository;
 
 	public void inserir(SecurityContext context, Ordem ordem) {
+		Optional<Usuario> userOptinal = Usuario.findByIdOptional(ordem.getUserId());
+		
+		Usuario usuario = userOptinal.orElseThrow();
+		
+		if(!usuario.getUsername().equals(context.getUserPrincipal().getName())) {
+			throw new RuntimeException("O usuário logado é diferente do userId");
+		}
+		
 		ordem.setData(LocalDate.now());
 		ordem.setStatus("ENVIADO");
 		
